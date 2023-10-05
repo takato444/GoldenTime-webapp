90c271a7c6e93692f125fcab6c1ceb46e06e127a
11136012 <129148174+hyy54@users.noreply.github.com>
2023-10-05 13:40:24 +0800

    修復port問題

--- Goldentime/settings.py
+++ Goldentime/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-0jkw&4^r_t^y)a=%yg$rydkaaolg7mre93p4gd$x6_eh_86(u4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['140.131.114.155']
-
+ALLOWED_HOSTS = []
 
 
 # Application definition
