d48df954dc6790a638e55ef0f37c39aeab7a2b7c
hyy54 <11136013@ntub.edu.tw>
2023-11-07 17:18:47 +0800

    清除一些垃圾

--- baseapp/views.py
+++ baseapp/views.py
@@ -27,28 +27,23 @@ def image_search(request):
 	pass
 def pHash(imgfile): #phash演算法
 	"""get image pHash value"""
-	#加载并调整图片为32x32灰度图片
 	img=cv2.imread(imgfile, cv2.CV_LOAD_IMAGE_GRAYSCALE)
 	img=cv2.resize(img,(32,32),interpolation=cv2.INTER_CUBIC)
- 
-	#创建二维列表
+
 	h, w = img.shape[:2]
 	vis0 = np.zeros((h,w), np.float32)
-	vis0[:h,:w] = img       #填充数据
- 
-	#二维Dct变换
+	vis0[:h,:w] = img      
+
 	vis1 = cv2.dct(cv2.dct(vis0))
-	#cv.SaveImage('a.jpg',cv.fromarray(vis0)) #保存图片
+
 	vis1.resize(8,8)
- 
-	#把二维list变成一维list
+
 	img_list=flat_gen(vis1.tolist()) 
  
-	#计算均值
+
 	avg = sum(img_list)*1./len(img_list)
 	avg_list = ['0' if i<avg else '1' for i in img_list]
- 
-	#得到哈希值
+
 	return ''.join(['%x' % int(''.join(avg_list[x:x+4]),2) for x in range(0,64,4)])
 def flat_gen(x): #flatten
     def iselement(e):
